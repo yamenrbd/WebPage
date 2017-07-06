acc41a906aaef35620661b743d7e7c5d43a09806
yamen <yamenrbd@hotmail.com>
2017-07-06 18:52:36 +0300

    two

---
+++ webPage_1/includes/connection.php
@@ -0,0 +1,20 @@
+<?php
+
+class connection{
+    private $host ="localhost";
+    private $user="root";
+    private $password="";
+    private $database="webPageDB";
+    
+    function connect(){
+    $link = new mysqli($host,$user,$password,$database);
+    
+    if($link->connect_error){
+        die("connection failed ".$link->connect_error);
+    }
+    else{
+        echo "connection success";
+    return $link;
+    }
+    }
+}
