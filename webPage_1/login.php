bc1f188eaed71cd6bcd8fb7501ce82e26f626321
yamen <yamenrbd@hotmail.com>
2017-07-07 15:06:26 +0300

    three

--- webPage_1/login.php
+++ webPage_1/login.php
@@ -20,8 +20,24 @@
         <div class="container-fluid">
             <div class="row" id="head1">
                 <span id="headLog">German Center Services</span>
-               
+            </div>
+            <div id="logInFrom" class="row">
                 
+                <fieldset>
+                    <legend>User login </legend>
+                 <form >
+                    <div class="form-group">
+                        <label for="user">User Name:</label>
+                        <input type="text" class="form-control" id="user">
+                    </div>
+                    <div class="form-group">
+                    <label for="pwd">Password:</label>
+                    <input type="password" class="form-control" id="pwd">
+                    </div>
+  
+                    <button type="submit" class="btn btn-default">sign in</button>
+                    </form> 
+                    </fieldset>
             </div>
             
         
